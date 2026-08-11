952d5a889a2d1bceb576e6031ad034f78f90403e
stepan <stepanmuller@outlook.com>
2026-08-11 02:36:30 +0200

    working

--- cases/M40Full/M40Main.cu
+++ cases/M40Full/M40Main.cu
@@ -3,13 +3,13 @@ static constexpr int WALL_REFINEMENT_COUNT = 2; // min 2
 static constexpr int MEMORY_RESERVE_PERCENTAGE = 10;
 static constexpr int MEMORY_RESERVE_PERCENTAGE_INTERFACE = 10;
 
-static constexpr int MOVING_BOUNCEBACK_UPDATE_PERIOD = 8;
+static constexpr int MOVING_BOUNCEBACK_UPDATE_PERIOD = 1;
 static constexpr int GRID_REBUILD_PERIOD = 24;
 
 static constexpr int GRID_LEVEL_COUNT = 1;
 
 int reportChunk = 31;
-int plotterChunk = 2000;
+int plotterChunk = 1000;
 constexpr int iterationCount = 30000;
 
 constexpr float resGlobal = 0.1f; 														// mm
